889358761b272bc2062be1adb1460ec85f5a2927
Dyuti Mengji <mengjidyuti@gmail.com>
2025-05-23 06:28:10 -0700

    view post in detail feature + UUID

--- rest-class/index.js
+++ rest-class/index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+const { v4: uuidv4 } = require('uuid');
+
+
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
@@ -11,15 +14,18 @@ app.set("views", path.join(__dirname,"views"))
 app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
-    {
+    {   
+        id:uuidv4(),
         username: "DyutiMengji",
         content: "I will become better at this!"
     },
     {
+        id:uuidv4(),
         username: "DhanushMengji",
         content: "I love doing this!"
     },
-    {
+    {   
+        id:uuidv4(),
         username: "SrilathIndurthi",
         content: "We will achieve this!"
     }
@@ -36,9 +42,20 @@ app.get("/posts/new", (req, res)=>{
 app.post("/posts", (req, res)=>{
     // console.log(req.body);
     let {username, content} = req.body;
-    posts.push({username, content})
-    res.send("post request working");
+    let id = uuidv4();
+    posts.push({id, username, content})
+    res.redirect("/posts"); //by default get request
 });
+
+app.get("/posts/:id", (req, res)=>{
+    let {id} = req.params;
+    console.log(id);
+
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    res.render("show.ejs",{post});
+});
+
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
