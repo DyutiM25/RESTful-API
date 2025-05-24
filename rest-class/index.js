db168afa0f30715748a5880933ed3a295a2d731d
Dyuti Mengji <mengjidyuti@gmail.com>
2025-05-24 07:32:37 -0700

    delete post

--- rest-class/index.js
+++ rest-class/index.js
@@ -76,6 +76,13 @@ app.get("/posts/:id/edit", (req, res)=>{
     res.render("edit.ejs",{post});
 });
 
+app.delete("/posts/:id", (req, res)=>{
+    let {id} = req.params;
+      posts = posts.filter((p) => id !== p.id);
+    //   res.send("delete success");
+      res.redirect("/posts");
+});
+
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 });
