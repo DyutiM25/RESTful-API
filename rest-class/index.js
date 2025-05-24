3e0170719014f77b3c4f5edc8e539dcb9446cac0
Dyuti Mengji <mengjidyuti@gmail.com>
2025-05-23 20:32:29 -0700

    post-update functionality added

--- rest-class/index.js
+++ rest-class/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 const { v4: uuidv4 } = require('uuid');
 
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({extended:true}));
 
@@ -56,6 +58,23 @@ app.get("/posts/:id", (req, res)=>{
     res.render("show.ejs",{post});
 });
 
+app.patch("/posts/:id", (req, res)=>{
+    let {id} = req.params;
+    console.log(id);
+    let newContent = req.body.content;
+    console.log(newContent);
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post)
+    // res.send("patch request sent");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs",{post});
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
